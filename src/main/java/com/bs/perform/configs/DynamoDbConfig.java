b7cae3d2e04878f8a861def473f8aa1c420fdea1
hyojeong <khj444453@gmail.com>
2023-06-05 21:46:30 +0900

    DynamoDB connection PoC - DynamoDbConfig에 크리덴셜 정보 삭제

--- src/main/java/com/bs/perform/configs/DynamoDbConfig.java
+++ src/main/java/com/bs/perform/configs/DynamoDbConfig.java
@@ -1,10 +1,7 @@
 package com.bs.perform.configs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -12,20 +9,12 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 @Configuration
 public class DynamoDbConfig {
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String awsAccessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String awsSecretKey ;
+    private String region = "ap-northeast-2";
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
         DynamoDbClient build = DynamoDbClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKey, awsSecretKey)))
                 .build();
         return build;
     }
