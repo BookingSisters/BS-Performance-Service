a50c3dce48371a23b9fb485013d648cf8a24ebe1
hyojeong <khj444453@gmail.com>
2023-06-26 15:01:45 +0900

    refactor: DynamoDbTable객체를 싱글톤으로 관리되도록 변경

--- src/main/java/com/bs/perform/configs/DynamoDbConfig.java
+++ src/main/java/com/bs/perform/configs/DynamoDbConfig.java
@@ -1,8 +1,11 @@
 package com.bs.perform.configs;
 
+import com.bs.perform.models.Performance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
@@ -13,16 +16,20 @@ public class DynamoDbConfig {
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
-        DynamoDbClient build = DynamoDbClient.builder()
+        return DynamoDbClient.builder()
                 .region(Region.of(region))
                 .build();
-        return build;
     }
 
     @Bean
     public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
         return DynamoDbEnhancedClient.builder()
                 .dynamoDbClient(dynamoDbClient).build();
+    }
+
+    @Bean
+    public DynamoDbTable<Performance> performanceTable(DynamoDbEnhancedClient dynamoDbEnhancedClient) {
+        return dynamoDbEnhancedClient.table("BS-Performance", TableSchema.fromBean(Performance.class));
     }
 
 }
